b99d73e47f3f77e87953e76726bd9ae5ccafe913
LAURA NORA AZEVEDO <laura.azevedo10@etec.sp.gov.br>
2025-08-08 12:23:43 -0300

    PRIMEIROOO

--- App.js
+++ App.js
@@ -1,10 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Image, Button, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+        source={require('./assets/img/casual_dog.png')}
+        style={{ width: 200, height: 200 }}
+      />
+      
+      <Text>Ótimo dia!</Text>
+      <Text>Como deseja acessar?</Text>
+
+      <TouchableOpacity 
+        style={{backgroundColor: 'green', padding: 10 }}
+        onPress={() => alert('Você acessou.')}
+      >
+        <Image
+          source={require('./assets/img/Google.png')}
+          style={{ width: 20, height: 20 }}
+        />
+
+        <Text style={{ color: 'white' }}>Como deseja acessar?</Text>
+      </TouchableOpacity >
+
+      <TouchableOpacity 
+        style={{backgroundColor: 'white', borderColor: 'green', borderWidth: 2, borderradius: 8, width:350, height:60, margin: 30, justifyContent: 'center', alignItems: 'center'}}
+        onPress={() => alert('outras opções.')}>
+        <Text style={{ color: 'black' }}>Outras opções</Text>
+      </TouchableOpacity>
+
       <StatusBar style="auto" />
     </View>
   );
