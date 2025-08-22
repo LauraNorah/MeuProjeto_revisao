da6c4e263e59d68136be889a9b49817023cea0e1
LauraNorah <laura.nora.azevedo@gmail.com>
2025-08-22 07:27:49 -0300

    segundoo commit

--- App.js
+++ App.js
@@ -7,26 +7,26 @@ export default function App() {
     <View style={styles.container}>
       <Image
         source={require('./assets/img/casual_dog.png')}
-        style={{ width: 200, height: 200 }}
+        style={{ width: 470, height: 380 }}
       />
       
-      <Text>Ótimo dia!</Text>
-      <Text>Como deseja acessar?</Text>
+      <Text  style={{ fontSize: 40, fontStyle: 'bold', marginTop: 40}} > Ótimo dia!</Text>
+      <Text style={{ fontSize: 15, fontStyle: 'bold', marginTop: 10, marginBottom: 40}} >Como deseja acessar?</Text>
 
       <TouchableOpacity 
-        style={{backgroundColor: 'green', padding: 10 }}
+        style={{backgroundColor: 'rgb(20, 200, 113)', borderColor: 'rgb(98, 218, 160)', borderWidth: 2, borderradius: 8, width:350, height:55, margin: 5, justifyContent: 'center', alignItems: 'center' }}
         onPress={() => alert('Você acessou.')}
       >
         <Image
           source={require('./assets/img/Google.png')}
-          style={{ width: 20, height: 20 }}
+          style={{ width: 30, height: 30 }}
         />
 
         <Text style={{ color: 'white' }}>Como deseja acessar?</Text>
       </TouchableOpacity >
 
       <TouchableOpacity 
-        style={{backgroundColor: 'white', borderColor: 'green', borderWidth: 2, borderradius: 8, width:350, height:60, margin: 30, justifyContent: 'center', alignItems: 'center'}}
+        style={{backgroundColor: 'white', borderColor: 'rgb(98, 218, 160)', borderWidth: 2, borderradius: 8, width:350, height:55, margin: 5, justifyContent: 'center', alignItems: 'center'}}
         onPress={() => alert('outras opções.')}>
         <Text style={{ color: 'black' }}>Outras opções</Text>
       </TouchableOpacity>
@@ -39,7 +39,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f3f3f3',
     alignItems: 'center',
     justifyContent: 'center',
   },
